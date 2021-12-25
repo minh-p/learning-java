f067a216c397466eff905b25f2f4a578c82a8e95
minh-p <minhisverynormal@gmail.com>
2021-12-25 13:21:10 -0800

    Hello World App

---
+++ HelloWorldApp/main.java
@@ -0,0 +1,14 @@
+/*
+    Syntax has the same as C's.
+    This is OOP though.
+    I can see that System is an object.
+    I am going to need some documentation for stuff though.
+    I understand the syntax and things at least.
+    There might be some special things that java has.
+*/
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
